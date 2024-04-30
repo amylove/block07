751525f3640f6cfe81678c33c47b9051a261b8bd
Amy Love <amy.love@gmail.com>
2024-04-29 22:26:03 -0500

    Creating variables and doing math for block07 assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+// Combo is 10 - 40 - 39
+
+// First Number is 10
+let firstNum = 75 - 65
+
+// Second number is 40
+let secondNum = 160 / 4
+
+// Third number is 39
+let thirdNum = 57 - 18
+
+
+let theTask = "You have received this message because you have been chosen to open an important vault. "
+let theCombo = `${firstNum}-${secondNum}-${thirdNum}` 
+let fullMsg = theTask + 'Here is the secret combination: ' + theCombo + '!'
+
+console.log(theCombo)
+console.log(fullMsg)
+
+alert(fullMsg)
+
